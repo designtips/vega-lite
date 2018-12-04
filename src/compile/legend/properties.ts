2d998918df504954e0e587e0f738b348ff3d6f1f
Kanit Wongsuphasawat <kanitw@gmail.com>
2018-12-04 00:15:39 -0800

    [WIP] prototype smart rules for legend

--- src/compile/legend/properties.ts
+++ src/compile/legend/properties.ts
@@ -1,8 +1,8 @@
 import {LabelOverlap} from 'vega';
 import {FieldDef, valueArray} from '../../fielddef';
-import {Legend} from '../../legend';
+import {Legend, LegendConfig} from '../../legend';
 import {hasContinuousDomain, ScaleType} from '../../scale';
-import {contains} from '../../util';
+import {contains, getFirstDefined} from '../../util';
 
 export function values(legend: Legend, fieldDef: FieldDef<string>) {
   const vals = legend.values;
@@ -18,6 +18,33 @@ export function clipHeight(scaleType: ScaleType) {
     return 20;
   }
   return undefined;
+}
+
+export function defaultGradientLength(legend: Legend, legendConfig: LegendConfig) {
+  const {
+    gradientDirection,
+    gradientHorizontalMaxLength,
+    gradientHorizontalMinLength,
+    gradientVerticalMaxLength,
+    gradientVerticalMinLength
+  } = legendConfig;
+
+  const direction = getFirstDefined(legend.direction, gradientDirection);
+
+  if (direction === 'horizontal') {
+    const orient = getFirstDefined(legend.orient, legendConfig.orient);
+    if (orient === 'left' || orient === 'right') {
+      return gradientHorizontalMinLength;
+    } else {
+      return gradientLengthSignal('width', gradientHorizontalMinLength, gradientHorizontalMaxLength);
+    }
+  } else {
+    return gradientLengthSignal('height', gradientVerticalMinLength, gradientVerticalMaxLength);
+  }
+}
+
+function gradientLengthSignal(size: 'width' | 'height', min: number, max: number) {
+  return {signal: `min(max(${size}, ${min}), ${max})`};
 }
 
 export function labelOverlap(scaleType: ScaleType): LabelOverlap {
